0498d80862bdff78b8d914aaf120d172ac1a02b6
Louis Brunner <louis.brunner.fr@gmail.com>
2017-04-13 12:46:26 +0100

    More modern examples

--- src/examples/App.js
+++ src/examples/App.js
@@ -1,25 +1,27 @@
-import React, { createClass } from 'react';
+import React, { PureComponent } from 'react';
 import { DragDropContext } from 'react-dnd';
 import MultiBackend, { Preview } from 'lib/index.js';
 import Card from './Card';
 import Basket from './Basket';
 
-const App = createClass({
-  generatePreview: function (type, item, style) {
+class App extends PureComponent {
+  generatePreview(type, item, style) {
     Object.assign(style, {backgroundColor: item.color, width: '50px', height: '50px'});
     return <div style={style}>Generated</div>;
-  },
-
-  render: function () {
-    return  <div>
-              <Card color="#cc2211" />
-              <Card color="#22cc11" />
-              <Card color="#2211cc" />
-              <Basket />
-              <Preview generator={this.generatePreview} />
-              <div id="console" />
-            </div>;
   }
-});
 
-module.exports = DragDropContext(MultiBackend)(App);
+  render() {
+    return (
+      <div>
+        <Card color="#cc2211" />
+        <Card color="#22cc11" />
+        <Card color="#2211cc" />
+        <Basket />
+        <Preview generator={this.generatePreview} />
+        <div id="console" />
+      </div>
+    );
+  }
+}
+
+export default DragDropContext(MultiBackend)(App);
