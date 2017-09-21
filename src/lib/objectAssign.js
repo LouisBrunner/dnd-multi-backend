7deed1dfc634197c89f6cdfec8548dbc7dd4d34e
David Losert <david.losert@virtual-identity.com>
2017-09-21 15:51:27 +0200

    Replaced all "for ... of" loops with native array-methods to avoid Symbol-creation by babel and therefore errors in IE

--- src/lib/objectAssign.js
+++ src/lib/objectAssign.js
@@ -1,10 +1,10 @@
 export default function(target, ...sources) {
-  for (const source of sources) {
+  sources.forEach((source) => {
     for (const name in source) {
       if (Object.prototype.hasOwnProperty.call(source, name)) {
         target[name] = source[name];
       }
     }
-  }
+  });
   return target;
 }
