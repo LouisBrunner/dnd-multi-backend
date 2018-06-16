af877c34b589a6f1fe71dbeb783449af0fd8f9c9
Louis Brunner <louis.brunner.fr@gmail.com>
2018-06-16 20:05:08 +0100

    fix some tests

--- packages/react-dnd-multi-backend/src/__tests__/Preview_spec.js
+++ packages/react-dnd-multi-backend/src/__tests__/Preview_spec.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import { mount } from 'enzyme';
 
+import DnDPreview from 'react-dnd-preview';
 import Preview from '../Preview';
 
 describe('Preview component', () => {
-  const createComponent = ({preview = false, isDragging = false, currentOffset = null, generator = jest.fn(), item = {}, itemType = ''} = {}) => {
+  const createComponent = ({preview = false, generator = jest.fn()} = {}) => {
     const context = {
       dragDropManager: {
-        getMonitor: () => {
-          return {
-            getSourceClientOffset: () => { return currentOffset; },
-            isDragging: () => { return isDragging; },
-            getItemType: () => { return itemType; },
-            getItem: () => { return item; },
-            subscribeToOffsetChange: () => {},
-            subscribeToStateChange: () => {},
-          };
-        },
         getBackend: () => {
           return {
             previewEnabled: () => { return preview; },
@@ -29,45 +20,20 @@ describe('Preview component', () => {
 
   test('is a PureComponent', () => {
     const component = createComponent();
-    expect(component.name()).toBe('DragLayer(Preview)');
-    expect(component.instance()).toBeInstanceOf(Preview);
+    expect(component.name()).toBe('Preview');
+    expect(component.instance()).toBeInstanceOf(React.PureComponent);
   });
 
-  test('no preview and no DnD', () => {
+  test('is empty (no preview)', () => {
     const component = createComponent();
     expect(component.html()).toBeNull();
   });
 
-  test('no preview and DnD', () => {
-    const component = createComponent({isDragging: true, currentOffset: {x: 1000, y: 2000}});
-    expect(component.html()).toBeNull();
-  });
-
-  test('preview and no DnD', () => {
-    const component = createComponent({preview: true});
-    expect(component.html()).toBeNull();
-  });
-
-  test('preview and DnD', () => {
-    const generator = (type, item, style) => {
-      return <div style={style}>{item.coucou}: {type}</div>;
-    };
+  test('is not empty (preview)', () => {
     const component = createComponent({
-      item: {coucou: 'dauphin'},
-      itemType: 'toto',
       preview: true,
-      isDragging: true, currentOffset: {x: 1000, y: 2000},
-      generator,
+      generator: () => {},
     });
-    expect(component.html()).not.toBeNull();
-    const div = component.find('div');
-    expect(div).toExist();
-    expect(div).toHaveText('dauphin: toto');
-    expect(div).toHaveStyle('pointerEvents', 'none');
-    expect(div).toHaveStyle('position', 'fixed');
-    expect(div).toHaveStyle('top', 0);
-    expect(div).toHaveStyle('left', 0);
-    expect(div).toHaveStyle('transform', 'translate(1000px, 2000px)');
-    expect(div).toHaveStyle('WebkitTransform', 'translate(1000px, 2000px)');
+    expect(component.find(DnDPreview)).toExist();
   });
 });
