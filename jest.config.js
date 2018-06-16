901846a44936b4afa2bc2647883bbb6b0335d7a0
Louis Brunner <louis.brunner.fr@gmail.com>
2018-06-16 15:13:38 +0100

    Lint jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  setupTestFrameworkScriptFile: "<rootDir>/tests/setup.js",
+  setupTestFrameworkScriptFile: '<rootDir>/tests/setup.js', // eslint-disable-line id-length
   collectCoverage: true,
-  coverageReporters: process.env.CI ? ["lcov"] : ["text", "text-summary", "html"],
+  coverageReporters: process.env.CI ? ['lcov'] : ['text', 'text-summary', 'html'], // eslint-disable-line no-process-env
   collectCoverageFrom: [
-    "packages/*/src/**/*.js",
+    'packages/*/src/**/*.js',
   ],
 };
