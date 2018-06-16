2acede73a6ac657c199fd81d310406269148d595
Louis Brunner <louis.brunner.fr@gmail.com>
2018-06-16 21:30:08 +0100

    fix some tests

--- packages/react-dnd-preview/src/__tests__/index_spec.js
+++ packages/react-dnd-preview/src/__tests__/index_spec.js
@@ -1,35 +1,35 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import TestBackend from 'react-dnd-test-backend';
+import { DragDropContext, DragSource } from 'react-dnd';
 
 import Preview from '../index';
 
-describe('Preview component', () => {
-  const createComponent = ({isDragging = false, currentOffset = null, generator = jest.fn(), item = {}, itemType = ''} = {}) => {
-    const context = {
-      dragDropManager: {
-        getMonitor: () => {
-          return {
-            getSourceClientOffset: () => { return currentOffset; },
-            isDragging: () => { return isDragging; },
-            getItemType: () => { return itemType; },
-            getItem: () => { return item; },
-            subscribeToOffsetChange: () => {},
-            subscribeToStateChange: () => {},
-          };
-        },
-      },
-    };
-    return mount(<Preview generator={generator} />, {context});
+describe('Preview subcomponent', () => {
+  const createComponent = ({generator = jest.fn(), source = null} = {}) => {
+    @DragDropContext(TestBackend)
+    class TestRoot extends React.Component {
+      render() {
+        return (
+          <div>
+            {source}
+            <Preview generator={generator} />
+          </div>
+        );
+      }
+    }
+
+    return mount(<TestRoot />);
   };
 
   test('is a DragLayer-decorated Preview', () => {
-    const component = createComponent();
+    const component = createComponent().find(Preview);
     expect(component.name()).toBe('DragLayer(Preview)');
     expect(component.instance()).toBeInstanceOf(Preview);
   });
 
   test('is null when DnD is not in progress', () => {
-    const component = createComponent();
+    const component = createComponent().find(Preview);
     expect(component.html()).toBeNull();
   });
 
@@ -37,12 +37,30 @@ describe('Preview component', () => {
     const generator = (type, item, style) => {
       return <div style={style}>{item.coucou}: {type}</div>;
     };
-    const component = createComponent({
-      item: {coucou: 'dauphin'},
-      itemType: 'toto',
-      isDragging: true, currentOffset: {x: 1000, y: 2000},
-      generator,
+    const Source =
+      @DragSource('toto', {
+        beginDrag: () => { return {coucou: 'dauphin'}; },
+        canDrag: () => { return true; },
+      }, (connect) => {
+        return {connectDragSource: connect.dragSource()};
+      })
+      class DS extends React.Component {
+        render() { return this.props.connectDragSource(<div />); }
+      }
+    ;
+
+    const root = createComponent({source: <Source />, generator});
+
+    const backend = root.instance().getManager().getBackend();
+    backend.simulateBeginDrag([root.find(Source).instance().getHandlerId()], {
+      clientOffset: {x: 1, y: 2},
+      getSourceClientOffset: () => {
+        return {x: 1000, y: 2000};
+      },
     });
+    root.update();
+
+    const component = root.find(Preview);
     expect(component.html()).not.toBeNull();
     const div = component.find('div');
     expect(div).toExist();
