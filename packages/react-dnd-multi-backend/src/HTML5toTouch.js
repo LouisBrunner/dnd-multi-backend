c111ac75afca822523456f46914bb44abe5f791f
Louis Brunner <louis.brunner.fr@gmail.com>
2020-05-30 13:21:26 +0100

    Update dependencies, fix imports for react-dnd 11, fixes #49

--- packages/react-dnd-multi-backend/src/HTML5toTouch.js
+++ packages/react-dnd-multi-backend/src/HTML5toTouch.js
@@ -1,5 +1,5 @@
-import HTML5Backend from 'react-dnd-html5-backend';
-import TouchBackend from 'react-dnd-touch-backend';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { TouchBackend } from 'react-dnd-touch-backend';
 
 import { TouchTransition, MouseTransition } from 'dnd-multi-backend';
 
