34069de090646c6b793d138838f4ff10a52206c7
Hanh Le <hanh.le@employmenthero.com>
2020-09-17 12:34:45 +0700

    Fix formatter problem

--- packages/react-dnd-preview/src/usePreview.js
+++ packages/react-dnd-preview/src/usePreview.js
@@ -3,24 +3,26 @@ import { useDragLayer } from 'react-dnd';
 const getStyle = (currentOffset) => {
   const transform = `translate(${currentOffset.x}px, ${currentOffset.y}px)`;
   return {pointerEvents: 'none', position: 'fixed', top: 0, left: 0, transform, WebkitTransform: transform};
+const calculatePointerPosition = (monitor) => {
+  if (!monitor.getClientOffset() || !monitor.getInitialSourceClientOffset()) {
+    return null;
+  }
+  return {
+    x:
+      monitor.getClientOffset().x -
+      (monitor.getInitialClientOffset().x -
+        monitor.getInitialSourceClientOffset().x),
+    y:
+      monitor.getClientOffset().y -
+      (monitor.getInitialClientOffset().y -
+        monitor.getInitialSourceClientOffset().y),
+  };
 };
 
 const usePreview = () => {
   const collectedProps = useDragLayer((monitor) => {
     return {
-      currentOffset:
-        !monitor.getClientOffset() || !monitor.getInitialSourceClientOffset()
-          ? null
-          : {
-              x:
-                monitor.getClientOffset().x -
-                (monitor.getInitialClientOffset().x -
-                  monitor.getInitialSourceClientOffset().x),
-              y:
-                monitor.getClientOffset().y -
-                (monitor.getInitialClientOffset().y -
-                  monitor.getInitialSourceClientOffset().y),
-            },
+      currentOffset: calculatePointerPosition(monitor),
       isDragging: monitor.isDragging(),
       itemType: monitor.getItemType(),
       item: monitor.getItem(),
