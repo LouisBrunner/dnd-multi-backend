e16d0e65f538413414a293b0ab62857fb509738b
hanh090 <lehanh.bk.mt1002@gmail.com>
2020-10-02 22:58:58 +0700

    Change from source to client (#55)

--- packages/react-dnd-preview/src/usePreview.js
+++ packages/react-dnd-preview/src/usePreview.js
@@ -2,21 +2,40 @@ import { useDragLayer } from 'react-dnd';
 
 const getStyle = (currentOffset) => {
   const transform = `translate(${currentOffset.x}px, ${currentOffset.y}px)`;
-  return {pointerEvents: 'none', position: 'fixed', top: 0, left: 0, transform, WebkitTransform: transform};
+  return {
+    pointerEvents: 'none',
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    transform,
+    WebkitTransform: transform,
+  };
+};
+
+const calculatePointerPosition = (monitor) => {
+  if (!monitor.getClientOffset() || !monitor.getInitialSourceClientOffset()) {
+    return null;
+  }
+
+  return {
+    x: monitor.getClientOffset().x - (monitor.getInitialClientOffset().x - monitor.getInitialSourceClientOffset().x),
+    y: monitor.getClientOffset().y - (monitor.getInitialClientOffset().y - monitor.getInitialSourceClientOffset().y),
+  };
 };
 
 const usePreview = () => {
   const collectedProps = useDragLayer((monitor) => {
     return {
-      currentOffset: monitor.getSourceClientOffset(),
+      currentOffset: calculatePointerPosition(monitor),
       isDragging: monitor.isDragging(),
       itemType: monitor.getItemType(),
       item: monitor.getItem(),
+      monitor,
     };
   });
 
   if (!collectedProps.isDragging || collectedProps.currentOffset === null) {
-    return {display: false};
+    return { display: false };
   }
 
   return {
@@ -24,6 +43,7 @@ const usePreview = () => {
     itemType: collectedProps.itemType,
     item: collectedProps.item,
     style: getStyle(collectedProps.currentOffset),
+    monitor: collectedProps.monitor,
   };
 };
 
